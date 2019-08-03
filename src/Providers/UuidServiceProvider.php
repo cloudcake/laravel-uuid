1429ef679000727c278e04e214b1a53e1e4bd7d6
Stephen Lake <stephen@closurecode.com>
2019-08-03 05:36:08 +0200

    Add service provider

---
+++ src/Providers/UuidServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Larashim\Stopwatch\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UuidServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap Uuid.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
